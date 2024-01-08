2a7457f076c7c4179846e9034aa1e6d831691927
PrasannaGH963 <prasannabadgujar963@gmail.com>
2024-01-08 20:44:45 +0530

    trial commit

--- testbg.py
+++ testbg.py
@@ -4,19 +4,19 @@ import streamlit as st
 background_image = "your_image_url.jpg"  # Replace with the URL or local path of your image
 
 # Define custom CSS with the background image
-custom_css = f"""
-    <style>
-        body {{
-            background-image: url('{background_image}');
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }}
-    </style>
-"""
-
+# custom_css = f"""
+#     <style>
+#         body {{
+#             background-image: url('{background_image}');
+#             background-size: cover;
+#             background-repeat: no-repeat;
+#             background-attachment: fixed;
+#         }}
+#     </style>
+# """
+st.button("button")
 # Apply the custom CSS using st.markdown
-st.markdown(custom_css, unsafe_allow_html=True)
+# st.markdown(custom_css, unsafe_allow_html=True)
 
 # Rest of your Streamlit app code
 st.title("My Streamlit App")
